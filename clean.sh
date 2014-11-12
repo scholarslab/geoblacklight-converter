3746d0aa54119d0a71dfbff83b5dfcfb3be743f1
Wayne Graham <wayne.graham@virginia.edu>
2014-11-12 09:42:56 -0500

    Update deprecated functions and glob patterns
    Double quote strings to avoid glob splits
    convert  to

--- clean.sh
+++ clean.sh
@@ -19,7 +19,7 @@ GEOSERVER_ROOT="http://gis.lib.virginia.edu/geoserver"
 STACKS_ROOT="http://gis.lib.virginia.edu"
 
 make_dirs() {
-  echo -e "${COL_CYAN}Making data directories... $COL_RESET"
+  echo "${COL_CYAN}Making data directories... $COL_RESET"
   mkdir -p {$MODS_DIR,$GEOB_DIR,$DATA_DIR,$ISOS_DIR}
 }
 
@@ -27,19 +27,19 @@ breakout_layers() {
     for file in $DATA_DIR/*.xml; do
         # create partial path and filename for new files.
         # get just the filename with basename, and remove .xml from the end
-        ofn="$ISOS_DIR/`basename ${file%.xml}`"
+        ofn="$ISOS_DIR/$(basename "${file%.xml}")"
         if [ ! -r "$ofn" ]; then
-            echo -e "$COL_GREEN Breaking out ${COL_BLUE}layers$COL_GREEN from$COL_YELLOW ${file}$COL_GREEN to individual XML files.$COL_RESET"
-            xsltproc -stringparam baseName $ofn xslt/geonetwork2iso.xsl $file 
+            echo "$COL_GREEN Breaking out ${COL_BLUE}layers$COL_GREEN from$COL_YELLOW ${file}$COL_GREEN to individual XML files.$COL_RESET"
+            xsltproc -stringparam baseName "$ofn" xslt/geonetwork2iso.xsl "$file"
         fi
     done
 }
 
 convert_mods() {
   for file in $ISOS_DIR/*.xml; do
-    ofn="$MODS_DIR/`basename $file`"
+    ofn="$MODS_DIR/$(basename "$file")"
     if [ ! -r "$ofn" ]; then
-      echo  -e "$COL_GREEN Converting ${COL_RED}ISO$COL_GREEN for$COL_YELLOW ${ofn}$COL_GREEN to ${COL_CYAN}MODS $COL_RESET"
+      echo "$COL_GREEN Converting ${COL_RED}ISO$COL_GREEN for$COL_YELLOW ${ofn}$COL_GREEN to ${COL_CYAN}MODS $COL_RESET"
       xsltproc \
         xslt/iso2mods.xsl "$file" > "$ofn"
     fi
@@ -49,12 +49,12 @@ convert_mods() {
 convert_solr()
 {
   for file in $MODS_DIR/*.xml; do
-    ofn="$GEOB_DIR/`basename $file`"
+    ofn="$GEOB_DIR/$(basename "$file")"
     if [ ! -r "$ofn" ]; then
-      echo  -e "$COL_GREEN  Converting ${COL_CYAN}MODS$COL_GREEN for$COL_YELLOW ${ofn}$COL_GREEN to ${COL_MAGENTA}Solr $COL_RESET"
+      echo "$COL_GREEN  Converting ${COL_CYAN}MODS$COL_GREEN for$COL_YELLOW ${ofn}$COL_GREEN to ${COL_MAGENTA}Solr $COL_RESET"
       xsltproc \
         -stringparam geoserver_root $GEOSERVER_ROOT \
-        -stringparam now `date -u "+%Y-%m-%dT%H:%M:00Z"` \
+        -stringparam now "$(date -u "+%Y-%m-%dT%H:%M:00Z")" \
         -stringparam rights "Public" \
         -stringparam stacks_root $STACKS_ROOT \
         xslt/mods2geoblacklight.xsl "$file" > "$ofn"
@@ -63,7 +63,7 @@ convert_solr()
 }
 
 cleanup() {
-  echo -e "${COL_CYAN}Cleaning up old files... $COL_RESET"
+  echo "${COL_CYAN}Cleaning up old files... $COL_RESET"
   rm -f $DATA_DIR/*.xml
   rm -f $MODS_DIR/*.xml
   rm -f $GEOB_DIR/*.xml
